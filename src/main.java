e2cc7155c21a3df7570ef117ccbd33ea4d842866
даниил никулин <daniilnicula@gmail.com>
2022-03-19 14:48:12 +0300

    2

--- src/main.java
+++ src/main.java
@@ -1,60 +1,49 @@
-
 import javax.swing.*;
-import java.util.*;
+import java.awt.event.*;
 
 public class main extends JFrame
 {
-    // Данные для таблиц
-    private Object[][] array = new String[][] {{ "Сахар" , "кг", "1.5" },
-            { "Мука"  , "кг", "4.0" },
-            { "Молоко", "л" , "2.2" }};
-    // Заголовки столбцов
-    private Object[] columnsHeader = new String[] {"Наименование", "Ед.измерения",
-            "Количество"};
-    public main() {
-        super("Простой пример с JTable");
+    private DefaultListModel<String> dlm = new DefaultListModel<String>();
+    private final String[] data2 = {"Имя +78888888888", "   "};
+    public main()
+    {
+        super("телефонный справочник");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        // Простая таблица
-        JTable table1 = new JTable(array, columnsHeader);
-        // Таблица с настройками
-        JTable table2 = new JTable(3, 5);
-        // Настройка таблицы
-        table2.setRowHeight(30);
-        table2.setRowHeight(1, 20);
+        JPanel contents = new JPanel();
+        JTextField name1 =  new JTextField("имя",25);
+        JTextField name2 =  new JTextField("телефон", 25);
 
-        table2.setShowVerticalLines(false);
-
-        // Данные для таблицы на основе Vector
-        Vector<Vector<String>> data = new Vector<Vector<String>>();
-        // Вектор с заголовками столбцов
-        Vector<String> header = new Vector<String>();
-        // Формирование в цикле массива данных
-        for (int j = 0; j < array.length; j++) {
-            header.add((String)columnsHeader[j]);
-            Vector<String> row = new Vector<String>();
-            for (int i = 0; i < array[j].length; i++) {
-                row.add((String)array[j][i]);
-            }
-            data.add(row);
+        name1.setHorizontalAlignment(JTextField.LEFT);
+        name2.setHorizontalAlignment(JTextField.LEFT);
+        for (String string : data2) {
+            dlm.add(0, string);
         }
-        // Таблица на основе вектора
-        JTable table3 = new JTable(data, header);
-        // Размещение таблиц в панели с блочным расположением
-        Box contents = new Box(BoxLayout.Y_AXIS);
-        contents.add(new JScrollPane(table1));
-        contents.add(new JScrollPane(table2));
 
-        // Настройка таблицы table3 - цвет фона, цвет выделения
+        JButton add = new JButton("Добавить");
+        add.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                String name = name1.getText();
+                String number = name2.getText();
+                String data = name + " "+number;
 
-        // Скрытие сетки таблицы
-        table3.setShowGrid(false);
-        // contents.add(new JScrollPane(table3));
-        contents.add(table3);
-        // Вывод окна на экран
+                dlm.add(dlm.getSize(), data);
+                validate();
+                name1.setText("");
+                name2.setText("");
+            }
+        });
+
+        JList<String> list2 = new JList<String>(dlm);
+        contents.add(add);
+        contents.add(name1);
+        contents.add(name2);
+        contents.add(new JScrollPane(list2));
         setContentPane(contents);
-        setSize(500, 400);
+        setSize(400, 250);
         setVisible(true);
     }
+
     public static void main(String[] args) {
         new main();
     }
